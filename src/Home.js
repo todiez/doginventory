dc5cd7130acbdb2da3f9b1d5013f711b56ebe982
tybo-dev <tobias@diez.guru>
2023-01-23 21:00:26 +0100

    tests

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,24 @@
-import { useShelterContext } from "./utils/dogShelterContext"
-
+import { useShelterContext, ShelterContexts } from "./utils/dogShelterContext";
 
 function Home() {
-
   //getting the context inside this component
-  const {dogInventory} = useShelterContext();
+  const { dogInventory } = useShelterContext();
   console.log(dogInventory);
 
-    return (
-      <div className="home">
-        <h1>HOME PAGE</h1>
-      </div>
-    );
-  }
+  return (
+    <ShelterContexts.Consumer>
+      {(context) => {
+        // const { dogInv1 } = context;
+        // console.log(dogInv1);
+        return (
+          <div className="home">
+            <h1>HOME PAGE</h1>
+            <p>test: {dogInventory[0].name}</p>
+          </div>
+        );
+      }}
+    </ShelterContexts.Consumer>
+  );
+}
 
-  export default Home;
+export default Home;
