0d7d7c5a4b80996b76283f30f2f36ef64b642249
tybo-dev <tobias@diez.guru>
2023-01-23 21:15:42 +0100

    backwards

--- src/Home.js
+++ src/Home.js
@@ -5,20 +5,12 @@ function Home() {
   const { dogInventory } = useShelterContext();
   console.log(dogInventory);
 
-  return (
-    <ShelterContexts.Consumer>
-      {(context) => {
-        // const { dogInv1 } = context;
-        // console.log(dogInv1);
-        return (
+  return (       
           <div className="home">
             <h1>HOME PAGE</h1>
             <p>test: {dogInventory[0].name}</p>
           </div>
-        );
-      }}
-    </ShelterContexts.Consumer>
-  );
+        );  
 }
 
 export default Home;
